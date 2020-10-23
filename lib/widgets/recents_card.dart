2b52eaef90f7d403c6e51f87d455ef197885e350
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-10-23 17:23:21 +0100

    Added image list

--- lib/widgets/recents_card.dart
+++ lib/widgets/recents_card.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Recents extends StatelessWidget {
+  final int index;
+
+  Recents({this.index});
+
+  final images = [
+    'images/plant2.jpg',
+    'images/plant5.jpg',
+    'images/plant4.jpg',
+    'images/plant3.jpg',
+    'images/plant1.JPG',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,6 +28,10 @@ class Recents extends StatelessWidget {
             width: 100,
             margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(images[index]),
+                fit: BoxFit.fill,
+              ),
               color: Colors.grey,
               borderRadius: BorderRadius.circular(10),
             ),
